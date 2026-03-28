c9db83a52c3b96145dbc92997e641ef65a13410d
jneberhard <jreberhard3@gmail.com>
2026-03-27 21:08:23 -0600

    Mar 27 commit

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Overview
 
-{Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
+This program is an analysis of a dataset with 4,269 loan applications that were either approved or denied. This dataset includes some basic information such as number of dependents, graduated or not, self-employed or not, annual income, loan amount, how long the loan is (in years), fico score, hard assets (residential, commercial, luxury), cash, and approved or not.
+* [Web Site Name](https://www.kaggle.com/datasets/rohitgrewal/loan-approval-dataset) -- Dataset link
 
-{Provide a description of the data set that you are analyzing.  Include the link of where you obtained the data.}
 
-{Describe your purpose for writing this software to analyze the data.}
+I wanted to analyze if being self-employed made a difference and what percentage of loans go to people above median income.
+I also want to be able to put in a few preliminary numbers to see what the chance was of getting a loan.
 
 {Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the data set, the questions and answers, the code running and a walkthrough of the code.}
 
@@ -12,23 +13,42 @@
 
 # Data Analysis Results
 
-{List the questions and the answers you found by doing this analysis.}
+Question #1 -  Are self‑employed applicants approved at lower rates than salaried applicants?
+Answer:  The percentage of self_employed approved loans is 62.23%.
+The percentage of NOT self_employed approved loans is 62.20%.
+Basically makes no difference if you are self-employed
+
+Question #2 - What percentage of approved loans go to applicants with below‑median income?
+The percentage of loans to below-median income is 51.05%.
+Analysis, a little over half the loans were to those with below-median income. If we had a different dataset that had the loan reason we could dig deeper and see if these low income people were buying houses, cars, or what their loans are for.
+
+
+Question #3 - Given a borrower’s annual income, requested loan amount, FICO score, and assets, what is the probability of getting a loan?
+This is dependent on the inputs on what the result would be.
+
 
 # Development Environment
 
-{Describe the tools that you used to develop the software}
+The tools used to develop the software was Visual Studio Code as the development environment. I used the Python virtual environment so it can run cleanly. I used a CSV dataset containing loan application data to do the statistical analysis.
 
-{Describe the programming language that you used and any libraries.}
+The programming language I used was Python.
+The libraries I used was Pandas to load and clean the data and Numpy to build the model and log transformations.
+I wrote a custom regression implementation instead of using a library so I could learn more and dig into it deeper.
 
 # Useful Websites
 
 {Make a list of websites that you found helpful in this project}
-* [Web Site Name](http://url.link.goes.here)
-* [Web Site Name](http://url.link.goes.here)
+* [Web Site Name](https://www.kaggle.com/datasets/rohitgrewal/loan-approval-dataset)
+https://www.kaggle.com/datasets/arbaaztamboli/loan-approval-dataset
+* [Web Site Name](https://www.youtube.com/watch?v=DkjCaAMBGWM ) -- Learning Pandas for Data Analysis? Start Here
+* [Web Site Name](https://www.youtube.com/watch?v=EXIgjIBu4EU ) -- Learn Pandas in 30 Minutes - Python Pandas Tutorial
+* [Web Site Name](https://www.youtube.com/watch?v=2uvysYbKdjM) -- Complete Python Pandas Data Science Tutorial!
+* [Web Site Name](https://scikit-learn.org/stable/) -- Website for machine learning in python for predictive data analysis. Did reading about this library. Did not use because I wanted to write and understand my own code instead of using something already created. But, it is good to know there are libraries out there to lean on if needed.
+
 
 # Future Work
 
-{Make a list of things that you need to fix, improve, and add in the future.}
-* Item 1
-* Item 2
-* Item 3
+List of things that you need to fix, improve, and add in the future.
+* Add in figure payment with interest... ask for interest rate and compute monthly payment
+* Find more datasets, some with more information like loan purpose to see percentages for types of loans and median income.
+* More analysis, such as does having a college education make a difference?
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-#this will load that dataset
+#this will load that dataset ==  df means data or dataframe == conventional shorthand in python
 df = pd.read_csv("data/loan_approval_dataset.csv")
 df.columns = df.columns.str.strip() #stripping away the extra space before the text in the headers
 
 # Ensure numeric columns
 numeric_cols = [
+    'no_of_dependents',
     'income_annum',
     'loan_amount',
     'loan_term',
@@ -17,8 +18,31 @@ numeric_cols = [
     'luxury_assets_value',
     'bank_asset_value'
 ]
+
+#clean the data in each column so it is uniform
 df['income_annum'] = (
     df['income_annum']
+    .astype(str)
+    .str.replace(',', '', regex=False)
+    .str.strip()
+)
+
+df['loan_term'] = (
+    df['loan_term']
+    .astype(str)
+    .str.replace(',', '', regex=False)
+    .str.strip()
+)
+
+df['fico_score'] = (
+    df['fico_score']
+    .astype(str)
+    .str.replace(',', '', regex=False)
+    .str.strip()
+)
+
+df['loan_amount'] = (
+    df['loan_amount']
     .astype(str)
     .str.replace(',', '', regex=False)
     .str.strip()
@@ -31,6 +55,12 @@ df['loan_status'] = (
     .str.title()
 )
 
+df['no_of_dependents'] = (
+    df['no_of_dependents']
+    .astype(str)
+    .str.strip()
+)
+
 df['self_employed'] = (
     df['self_employed']
     .astype(str)
@@ -38,22 +68,56 @@ df['self_employed'] = (
     .str.title()
 )
 
+df['residential_assets_value'] = (
+    df['residential_assets_value']
+    .astype(str)
+    .str.strip()
+    .str.title()
+)
+
+df['commercial_assets_value'] = (
+    df['commercial_assets_value']
+    .astype(str)
+    .str.strip()
+    .str.title()
+)
+
+df['luxury_assets_value'] = (
+    df['luxury_assets_value']
+    .astype(str)
+    .str.strip()
+    .str.title()
+)
+
+df['bank_asset_value'] = (
+    df['bank_asset_value']
+    .astype(str)
+    .str.strip()
+    .str.title()
+)
+
+
+
+#convert numeric columns from str values to numbers
 for col in numeric_cols:
-    df[col] = pd.to_numeric(df[col], errors='coerce')
+    df[col] = pd.to_numeric(df[col], errors='coerce') # use coerce to convert to NaN if string can't be converted to a number
 
 # Drop rows with missing values in key columns
 df_clean = df.dropna(subset=['income_annum'])
 print()
 #finding the number of loans approved or rejected
 counts = df_clean['loan_status'].value_counts()
-print(f"Total loans approved or rejected: ")
+print("Total loans approved or rejected: ")
 approved = counts.get('Approved', 0)
 rejected = counts.get('Rejected', 0)
 print(f"Approved: {approved}")
 print(f"Rejected: {rejected}")
 print(f"Percentage of loans approved: {((approved / (approved + rejected)) * 100):.2f}%")
 
+########################################################################################################
 #question #1 -  Are self‑employed applicants approved at lower rates than salaried applicants?
+########################################################################################################
+
 print()
 print("QUESTION 1: Are self‑employed applicants approved at lower rates than salaried applicants?")
  #find self-employed - what % are approved
@@ -69,6 +133,7 @@ self_denied = len(df_clean[
 
 self_employed = (self_approved / (self_approved + self_denied)) * 100
 print(f"The percentage of self_employed approved loans is {self_employed:.2f}%.")
+
  #find salaried - what % are approved
 not_self_approved = len(df_clean[
     (df_clean['self_employed'] == 'No') &
@@ -83,8 +148,9 @@ not_self_denied = len(df_clean[
 not_self_employed = (not_self_approved / (not_self_approved + not_self_denied)) * 100
 print(f"The percentage of NOT self_employed approved loans is {not_self_employed:.2f}%.")
 
-
+########################################################################################################
 #question #2 - What percentage of approved loans go to applicants with below‑median income?
+########################################################################################################
 print()
 print("QUESTION 2: What percentage of approved loans go to applicants with below‑median income?")
 
@@ -109,16 +175,174 @@ percentage = (count_a / (count_a + count_b)) * 100
 print(f"The percentage of loans to below-median income is {percentage:.2f}%.")
 print()
 
-#question #3 - Given a borrower’s annual income, requested loan amount, and FICO score,
-# what is the estimated probability that their loan will be approved?
-income = float(input("What is your annual income? "))
-loan = float(input("What is your loan amount? "))
-fico = float(input("What is your FICO score? "))
-years = float(input("How many years to you want the loan? "))
+########################################################################################################
+#question #3 - Given a borrower’s annual income, requested loan amount, FICO score, and assets,
+# what is the probability of getting a loan?
+########################################################################################################
 
-#figure out percentage of income for the loan
-#total income through life of the loan
-income_percentage = (loan / (income * years)) * 100
-print(f"The percentage of your income to service the loan is {income_percentage:.2f}%.")
-monthly = (income_percentage * income) / 12 / 100
-print(f"Your monthly payment on principle is: ${monthly:.2f}.")
+#convert loan status to numeric for modeling
+df_clean['loan_status'] = df_clean['loan_status'].map({
+'Approved': 1,
+'Rejected': 0
+})
+
+#make income and loan ration matter
+df_clean['loan_to_income_ratio'] = df_clean['loan_amount'] / df_clean['income_annum']
+df_clean['high_loan_risk'] = (df_clean['loan_to_income_ratio'] > 4).astype(int)   # > 4 because that is about what real life situations are
+
+# make assets matter
+df_clean['total_assets'] = df_clean['luxury_assets_value'] + df_clean['bank_asset_value'] + df_clean['residential_assets_value'] + df_clean['commercial_assets_value']
+
+#omit the outliers
+df_clean['log_income'] = np.log1p(df_clean['income_annum'])
+df_clean['log_assets'] = np.log1p(df_clean['total_assets'])
+
+df_clean['loan_to_asset_ratio'] = df_clean['loan_amount'] / (df_clean['total_assets'] + 1)
+
+#create the data model
+feature_cols = ['loan_to_income_ratio', 'fico_score', 'loan_term', 'no_of_dependents', 'log_income', 'log_assets', 'loan_to_asset_ratio']
+X = df_clean[feature_cols].values  #the features
+y = df_clean['loan_status'].values   #the target
+
+#normalize/standardization
+means = X.mean(axis=0)   #calculates average for each column.
+standards = X.std(axis=0)  #standard deviation - how spread out the numbers are for each item
+standards[standards == 0] = 1
+X_scaled = (X - means) / standards  #subtracts the mean and divides by the standard deviation - standarization formula
+
+X_final = np.c_[np.ones(X_scaled.shape[0]), X_scaled] # adding the bios or intercept column
+
+
+#Make part of the dataset training and testing
+split_index = int(len(X_final) * 0.8)
+
+X_train = X_final[:split_index]
+X_test = X_final[split_index:]
+
+y_train = y[:split_index]
+y_test = y[split_index:]
+
+
+# sigmoid calculation
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+# compute weights - how much each feature influences the prediction
+def train_logistic(X, y, lr=0.1, epochs=1000):
+    weights = np.zeros(X.shape[1])
+    for _ in range(epochs):
+        preds = sigmoid(np.dot(X, weights))
+        error = preds - y
+        gradient = np.dot(X.T, error) / len(y)
+        weights -= lr * gradient
+    return weights
+
+weights = train_logistic(X_train, y_train)
+
+
+'''# --- 5. MODEL EVALUATION ---
+# 1. Get the model's raw scores for every row in the dataset
+z_all = np.dot(X_test, weights)
+# 2. Convert scores to probabilities (0.0 to 1.0)
+all_probs = sigmoid(z_all)
+# 3. Convert probabilities to a hard "Approved (1)" or "Rejected (0)"
+# We use 0.5 as the cutoff point
+predictions = (all_probs >= 0.5).astype(int)
+
+# 4. Calculate Accuracy (Percent of correct guesses)
+accuracy = (predictions == y_test).mean()
+
+# 5. Calculate "True Positives" (Model said Approved, and they WERE Approved)
+tp = np.sum((predictions == 1) & (y_test == 1))
+# 6. Calculate "False Positives" (Model said Approved, but they were actually REJECTED)
+fp = np.sum((predictions == 1) & (y_test == 0))
+# 7. Calculate "False Negatives" (Model said Rejected, but they were actually APPROVED)
+fn = np.sum((predictions == 0) & (y_test == 1))
+
+print("\n" + "="*30)
+print("MODEL PERFORMANCE SUMMARY")
+print("="*30)
+print(f"Overall Accuracy: {accuracy * 100:.2f}%")
+print(f"Total Correct:    {np.sum(predictions == y_test)} / {len(y_test)}")
+print(f"False Approvals:  {fp} (Dangerous for the bank!)")
+print(f"False Rejections: {fn} (Lost business!)")
+
+'''
+
+cont = True
+while cont:
+    # what is the estimated probability that their loan will be approved? User input
+    income = float(input("What is your annual income? "))
+    loan = float(input("What is your loan amount? "))
+    fico = float(input("What is your FICO score? "))
+    years = float(input("How many years to you want the loan? "))
+    dependents = float(input("How many dependents do you have? "))
+    asset = float(input("What is the value of your assets? "))
+    cash = float(input("How much cash on hand? "))
+
+    log_income = np.log1p(income)
+    log_assets = np.log1p(asset + cash)
+
+    loan_to_income_ratio = loan / (income + 1)
+    loan_to_asset_ratio = loan / (asset + cash + 1)
+
+    #normalize the input (just like the learning model was normailzed)
+    user_raw = np.array([loan_to_income_ratio, fico, years, dependents, log_income, log_assets, loan_to_asset_ratio])   #make sure same order as training model
+    user_scaled = (user_raw - means) / standards
+    user_final = np.insert(user_scaled, 0, 1)
+
+    #prediction model
+    z = np.dot(user_final, weights)
+    probability = sigmoid(z)
+
+    #figure monthly payment
+    monthly = loan / (years * 12)
+
+    #figure out percentage of income for the loan
+    monthly_income = income / 12
+    income_percentage = (monthly / monthly_income) * 100
+
+    #risk assesment
+    if loan_to_income_ratio < 2:
+        risk = "LOW"
+    elif loan_to_income_ratio < 4:
+        risk = "MODERATE"
+    else:
+        risk = "HIGH"
+
+    adjusted = probability
+
+    # FICO penalties - will lower the % the lower credit you have
+    if fico < 580:
+        adjusted *= 0.3   # very poor credit
+    elif fico < 620:
+        adjusted *= 0.5   # poor credit
+    elif fico < 670:
+        adjusted *= 0.75  # fair credit
+
+    # Loan-to-income penalties
+    if loan_to_income_ratio > 8:
+        adjusted *= 0.3
+    elif loan_to_income_ratio > 6:
+        adjusted *= 0.5
+    elif loan_to_income_ratio > 4:
+        adjusted *= 0.7
+
+    # Debt-to-income penalty (standard bank threshold is 43%)
+    if income_percentage > 43:
+        adjusted *= 0.6
+
+    # Cap it
+    adjusted = min(adjusted, 0.97)
+
+    #printing results
+    print("Results:")
+    print(f"- Risk Assessment: {risk}")
+    print(f"- Approval Probability: {adjusted * 100:.1f}%")
+    print()
+    print(f"The percentage of your income to service the loan is {income_percentage:.2f}%.")
+    print(f"Your monthly payment on principle is: ${monthly:.2f}.")
+    print()
+    again = input("Do you want to do another calculation? (Y/N)").strip().lower()
+    if again != "y":
+        cont = False
